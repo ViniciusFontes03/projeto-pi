3bbd569b1e686eb2ced96640eb2f66f8593b1b61
Vinicius Fontes <viniciusbf43@gmail.com>
2023-12-01 11:58:32 -0300

    Botao sair

--- app/controllers/Seguranca.java
+++ app/controllers/Seguranca.java
@@ -5,7 +5,6 @@ import play.mvc.Controller;
 
 public class Seguranca extends Controller {
 	
-	@Before
 	static void verificar() {
 		if (session.contains("usuario.email") == false) {
 			Login.form();
